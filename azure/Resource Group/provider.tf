3798962dcafff1a44d7037ffc088b42ebcdd448d
ratan <ratandhajal.dei@gmail.com>
2025-03-25 03:05:02 +0530

    push from local 1

--- azure/Resource Group/provider.tf
+++ azure/Resource Group/provider.tf
@@ -9,12 +9,6 @@ terraform {
 
 provider "azurerm" {
  features { }
-<<<<<<< HEAD
 subscription_id = "98174e88-da11-4352-adda-43babc144420" # Azure Subscription ID (Mandatory as per the vesrion mention above)
 client_id       = "f4b3b1b3-7b1d-4b3b-8b1b-3b7b1d5b5bfb" # Azure Service Principal App ID (Optional)
 }
-=======
-subscription_id = "98174e88-da11-4352-adda-43babc341410" # Azure Subscription ID
-client_id       = "f4b3b1b3-7b1d-4b3b-8b1b-3b7b1d5b5bfb" # Azure Service Principal App ID   
-}
->>>>>>> c6c0555 (correct the RG basic code)
